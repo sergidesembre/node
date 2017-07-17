f8fd18e692702777c5b00af380ef86e738d8e22a
Sergi Desembre <sergi.desembre@gmail.com>
2017-07-17 18:03:31 +0200

    Remove old project

--- component/weather/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const router = require('express').Router();
-const weatherController = require('./controller');
-
-router.route('/')
-    .get(weatherController.getAccuWeather);
-
-module.exports = router;
