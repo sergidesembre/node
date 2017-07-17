a40807d462da8b96ae0068c9f6886074b05e6a8e
Sergi Desembre <sergi.desembre@gmail.com>
2017-07-17 18:07:04 +0200

    Create new project

---
+++ server/waynabox/application/user/CreateUserRequest.js
@@ -0,0 +1,6 @@
+export default class CreateUserRequest {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+}
---
+++ server/waynabox/application/user/CreateUserUseCase.js
@@ -0,0 +1,8 @@
+//import CreateUserRequest from './CreateUserRequest';
+
+export default class CreateUserUseCase {
+  execute(req) {
+    console.log(req.username);
+//    req.password;
+  }
+}
